88542f050fe18d86d4f36b9654e810faa2d45bf2
AlexCabezas2018 <alexcabezas0705@gmail.com>
2018-12-12 15:57:50 +0100

    Terminado pero pendiente de aclaraciones

--- Problemas/Problemas de programacion dinamica/Patindromo/Patindromo.cpp
+++ Problemas/Problemas de programacion dinamica/Patindromo/Patindromo.cpp
@@ -11,28 +11,55 @@
 
 // función que resuelve el problema
 std::string patindromo(const std::string &word) {
-	int N = word.size();
+	int N = word.size() - 1;
 
 	std::vector<std::vector<int>> matriz(N + 1, std::vector<int>(N + 1, 0));
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			int k = N - j;
-			if (word[i - 1] == word[k]) {
-				matriz[i][j] = 1 + matriz[i - 1][j - 1];
+	for (int i = 1; i <= N - 1; i++){
+		matriz[i][i] = 1;
+	}
+
+	for (int d = 1; d <= N - 1; d++) {
+		for (int i = 1; i <= N - d; i++) {
+			int j = i + d;
+			if (word[i] == word[j]) {
+				matriz[i][j] = 2 + matriz[i + 1][j - 1];
 			}
 			else {
-				matriz[i][j] = std::max(matriz[i - 1][j], matriz[i][j - 1]);
+				matriz[i][j] = std::max(matriz[i + 1][j], matriz[i][j - 1]);
 			}
 		}
 	}
 
-	std::cout << matriz[N][N] << "\n";
+	int longMaxPalindromo = matriz[1][N];
+	std::string cadena(longMaxPalindromo + 1, ' ');
 
-	std::cout << "\n";
+	int iPorig = 1;
+	int jPorig = N;
+	int iCadena = 1;
+	int jCadena = longMaxPalindromo;
 
-
-	return " ";
+	while (longMaxPalindromo > 0){
+		if (word[iPorig] == word[jPorig]){
+			cadena[iCadena] = word[iPorig];
+			cadena[jCadena] = word[jPorig];
+			iCadena++; iPorig++;
+			jCadena--; jPorig--;
+			longMaxPalindromo -= 2;
+		}
+		else if (longMaxPalindromo == 1){
+			cadena[iCadena] = word[jPorig];
+			longMaxPalindromo--;
+		}
+		else if (word[iPorig] != word[jPorig]){
+			if (matriz[iPorig][jPorig - 1] > matriz[iPorig + 1][jPorig]){
+				jPorig--;
+			}
+			else iPorig++;
+		}
+	}
+	
+	return cadena.substr(1, cadena.size() - 1);
 
 }
 
@@ -46,7 +73,15 @@ bool resuelveCaso() {
     if (! std::cin)
         return false;
 
-	patindromo(word);
+	std::string sol;
+	if (word.size() == 1){
+		std::cout << word << "\n";
+	}
+	else {
+		word = " " + word;
+		sol = patindromo(word);
+		std::cout << sol << "\n";
+	}
 
 
     
